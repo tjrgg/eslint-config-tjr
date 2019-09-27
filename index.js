7dccb408dc1288df6f5ce936a9127d42e5960a41
Tyler <rocket0191@gmail.com>
2019-09-26 21:39:48 -0500

    feat: Add new rules

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ module.exports = {
 		es6: true
 	},
 	rules: {
+		'array-bracket-spacing': [ 'error', 'always' ],
 		'arrow-parens': [ 'error', 'always' ],
+		'badded-blocks': [ 'error', { 'blocks': 'never', 'classes': 'always', 'switches': 'never' } ],
 		'brace-style': [ 'error', 'stroustrup', { 'allowSingleLine': true } ],
 		'capitalized-comments': 'off',
 		'curly': [ 'warn', 'multi-line' ],
 		'func-names': [ 'warn', 'as-needed' ],
-		'linebreak-style': [ 'warn', 'unix' ]
+		'linebreak-style': [ 'warn', 'unix' ],
+		'no-multiple-empty-lines': [ 'error', { 'max': 3 } ],
+		'object-curly-spacing': [ 'error', 'always' ],
+		'quote-props': [ 'error', 'consistent' ],
+		'radix': [ 'error', 'as-needed' ],
+		'unicorn/filename-case': [ 'error', { 'cases': { 'camelCase': true, 'kebabCase': true, 'pascalCase': true } } ]
 	}
 };
