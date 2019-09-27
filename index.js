65b7e30d60d4df03ce944000a92e77713d1b4065
Tyler <rocket0191@gmail.com>
2019-09-26 20:34:07 -0500

    chore: Add initial config and rules

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	env: {
+		node: true,
+		es6: true
+	},
+	rules: {
+		'arrow-parens': [ 'error', 'always' ],
+		'brace-style': [ 'error', 'stroustrup', { 'allowSingleLine': true } ],
+		'capitalized-comments': 'off',
+		'curly': 'warn',
+		'func-names': [ 'warn', 'as-needed' ],
+		'linebreak-style': [ 'warn', 'unix' ]
+	}
+};
